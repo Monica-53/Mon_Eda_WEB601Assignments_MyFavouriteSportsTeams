b68d62db5be7b623bfc08ea6401160bded895cf8
Monica Eda <w0632746@myscc.ca>
2023-02-24 15:04:53 -0500

    used ngswitch but it did not workout

--- src/app/hover-affect.directive.ts
+++ src/app/hover-affect.directive.ts
@@ -1,32 +1,102 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
 
-  constructor(private elm: ElementRef) {
-  }
-
-  private addundeline() {
-    this.elm.nativeElement.style.textDecoration = 'underline';
+  constructor(private el: ElementRef) {
   }
 
 
-private removeunderline() {
-  this.elm.nativeElement.style.textDecoration = '';
+  @HostListener('mouseover') onMouseOver() {
+    this.el.nativeElement.style.textDecoration = 'underline';
+    this.el.nativeElement.style.fontWeight = 'bold';
+
+  }
+
+
+  @HostListener('mouseout') onMouseOut() {
+    this.el.nativeElement.style.textDecoration = 'none';
+    this.el.nativeElement.style.fontWeight = 'normal';
+
+  }
 }
 
-  private makeitbold() {
-    this.elm.nativeElement.style.textDecoration = 'bold';
-  }
-
-  private makeitnormal() {
-    this.elm.nativeElement.style.textDecoration = 'normal';
-  }
 
 
-}
+
+
+
+//   import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+//
+// @Directive({
+//   selector: '[Hover-Affect]'
+// })
+// export class HoverAffectDirective {
+//   constructor(private el:ElementRef) {
+//     this.affectType= 'type'
+//     this.affectType ='tags'
+//   }
+//   @Input('Hover-Affect') affectType: string;
+//
+//
+//
+//   @HostListener('mouseenter') onMouseEnter() {
+//     if (this.affectType === 'type') {
+//       this.el.nativeElement.style.textDecoration = 'underline';
+//     } else if (this. affectType === 'tag') {
+//       this.el.nativeElement.style.fontWeight = 'bold';
+//     }
+//   }
+//
+//   @HostListener('mouseleave') onMouseLeave() {
+//     if (this.affectType === 'type') {
+//       this.el.nativeElement.style.textDecoration = 'none';
+//     } else if (this.affectType === 'tag') {
+//       this.el.nativeElement.style.fontWeight = 'normal';
+//     }
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+// import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+//
+// @Directive({
+//   selector: '[Hover-Affect]'
+// })
+// export class HoverAffectDirective {
+//   constructor(private el: ElementRef ) {}
+//
+//   @Input() hoverStyle: string; // new input to specify which style property to change
+//
+//   @HostListener('mouseover') onMouseOver() {
+//     this.el.nativeElement.style[this.hoverStyle] = 'bold'; // change specified style to bold
+//   }
+//
+//   @HostListener('mouseout') onMouseOut() {
+//     this.el.nativeElement.style[this.hoverStyle] = 'normal'; // change specified style to normal
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
