fa7f304b614e41bbbfc2c4263df04c08243bc537
Monica Eda <w0632746@myscc.ca>
2023-03-06 10:47:03 -0500

    A new component "CreateContent"is added

--- src/app/create-content/create-content.component.ts
+++ src/app/create-content/create-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-create-content',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-content.component.scss']
 })
 export class CreateContentComponent {
-
+  letourPromise = new Promise((success, fail) => {
+    let testPass = true;
+    if (testPass) {
+      success("Success was achieved!");
+    } else {
+      fail("Failure :(");
+    }
+    ourPromise.then(successResult =>
+      console.log(successResult))
+      .catch(failResult => console.log(failResult))
+  })
 }
+
+
+
+
+
+
+
+
+
