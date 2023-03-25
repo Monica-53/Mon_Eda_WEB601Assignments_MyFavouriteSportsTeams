ab5842ffb58af8c5ea273b67b682c03c16beb953
Monica Eda <w0632746@myscc.ca>
2023-03-24 20:21:28 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-// import {SportService} from "./sport.service";
-// import {MessageService} from "./message.service";
+import {SportService} from "./sport.service";
+import {MessageService} from "./message.service";
 
 
 @Component({
@@ -10,18 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mon_Eda_MyFavouriteSportsTeams';
-  // constructor(private sportService: SportService,private messageService: MessageService) {
-//   }
-//
-//   getSport(id: number){
-//     let foundSport
-//     this.sportService.getSport(id).subscribe((sport)=>{
-// foundSport = sport;
-// this.messageService.add('Content Item at id: ' + 'idNumberHere"')
-//     });
-//     return foundSport;
-//
-//   }
+  constructor(private sportService: SportService,private messageService: MessageService) {
+  }
+
+  getSport(id: number){
+    let foundSport
+    this.sportService.getSport(id).subscribe((sport)=>{
+foundSport = sport;
+this.messageService.add('Content Item at id: ' + 'idNumberHere"')
+    });
+    return foundSport;
+
+  }
 }
 
 
