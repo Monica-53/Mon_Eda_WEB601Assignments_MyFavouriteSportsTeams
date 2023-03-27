dd6b154fdb124970d435e4ca876f512c7d4a2139
Monica Eda <w0632746@myscc.ca>
2023-03-27 12:20:00 -0400

    checked whether id is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,25 @@
 import { Component } from '@angular/core';
-import {SportService} from "./sport.service";
-import {MessageService} from "./message.service";
-
+import { SportService } from './sport.service';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Mon_Eda_MyFavouriteSportsTeams';
-  constructor(private sportService: SportService,private messageService: MessageService) {
-  }
+  constructor(
+    private sportService: SportService,
+    private messageService: MessageService
+  ) {}
 
-  getSport(id: number){
-    let foundSport
-    this.sportService.getSport(id).subscribe((sport)=>{
-foundSport = sport;
-this.messageService.add('Content Item at id: ' + 'idNumberHere"')
+  getSport(id: number) {
+    let foundSport;
+    this.sportService.getSport(id).subscribe((sport) => {
+      foundSport = sport;
+      this.messageService.add('Content Item at id: ' + id);
     });
     return foundSport;
-
   }
 }
-
-
